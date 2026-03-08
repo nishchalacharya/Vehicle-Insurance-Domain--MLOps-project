9ebb9916268f348713e748bfc26f58c0198c4471
Nishchal Sharma <nishchalsharma456@gmail.com>
2026-03-08 10:01:40 +0545

    project structure ready and venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Nishchal Sharma",
+    author_email="nishchalsharm456@gmail.com",
+    packages=find_packages()
+)
